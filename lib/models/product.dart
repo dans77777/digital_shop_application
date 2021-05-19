4e18465e9d07375563c6435f1eb63735480fe15d
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-05-19 18:43:02 +0530

    products model

---
+++ lib/models/product.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final double price;
+  final bool isFavourite;
+
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.imageUrl,
+      @required this.price,
+      @required this.isFavourite});
+}

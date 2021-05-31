9dc79c48e1b2789f7bf6cfd8c85ab37e0f65c2d3
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-05-31 08:33:25 +0530

    adding shop cart Data

---
+++ lib/providers/cart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+
+class CartItem {
+  String id;
+  String title;
+  double price;
+  int quantity;
+
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.price,
+      @required this.quantity});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existing) => CartItem(
+              id: existing.id,
+              title: existing.title,
+              price: existing.price,
+              quantity: existing.quantity + 1));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+  }
+}

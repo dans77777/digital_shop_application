c836593124c516ffac8423cb605750b03ebc43c2
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-06-02 17:53:42 +0530

    product -details added

--- lib/screens/productDetails_Screen.dart
+++ lib/screens/productDetails_Screen.dart
@@ -8,9 +8,38 @@ class ProductDetails extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments as String;
     final loaded = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loaded.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(loaded.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              child: Image.network(
+                loaded.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '\$${loaded.price}',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '${loaded.description}',
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        ));
   }
 }
